54220b215ab195dd000dd723c5e58e093f84a595
benjamin martinez <benjamartinez454@gmail.com>
2024-04-24 16:22:13 -0300

    Trabajo realizado al 100%

---
+++ bucles.js
@@ -0,0 +1,11 @@
+const arrays = [1, 2, 3];
+const contador = 0;
+
+while (arrays.length < 2) {
+  console.log("hola");
+  contador++;
+}
+
+for (let i = 0; i < arrays.length; i++) {
+  console.log("array", arrays[i]);
+}
---
+++ variables.js
@@ -0,0 +1,9 @@
+var name = "benja";
+var number = 16;
+var boolean = true;
+
+function funcion() {
+  let name2 = "benjamin";
+  console.log(name2);
+  console.log(nombre);
+}
