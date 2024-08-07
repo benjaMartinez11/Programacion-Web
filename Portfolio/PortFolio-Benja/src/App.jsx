5325cd04d99e893ee47e7c46c9f305e49ac3e542
benjamin martinez <benjamartinez454gmail.com>
2024-08-07 16:51:11 -0300

    agregue mi nombre y informacion

--- Portfolio/PortFolio-Benja/src/App.jsx
+++ Portfolio/PortFolio-Benja/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,26 +7,30 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<section class="bg-white dark:bg-gray-900">
+    <div class="container px-6 py-10 mx-auto">
+        <h1 class="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">PortFolio
+        </h1>
+
+
+        <div class="flex py-4 mt-4 overflow-x-auto overflow-y-hidden md:justify-center dark:border-gray-700">
+            <button
+                class="h-12 px-8 py-2 -mb-px text-sm text-center text-blue-600 bg-transparent border-b-2 border-blue-500 sm:text-base dark:border-blue-400 dark:text-blue-300 whitespace-nowrap focus:outline-none">
+                Redes Sociales
+            </button>          
+          
+        </div>
+
+        <section class="mt-8 space-y-8 lg:mt-12">
+            <section class="lg:flex lg:items-center">
+                <div class="lg:w-1/2 ">       
+                    <h2 class="mt-2 text-2xl font-semibold text-gray-800 capitalize dark:text-white"> Benjamin Martinez</h2>
+                </div>
+         
+            </section>
+        </section>
+    </div>
+</section>
     </>
   )
 }
