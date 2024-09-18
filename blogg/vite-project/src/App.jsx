afc931e9881e6e6e399671477eba41c07970eebf
benjamin martinez <benjamartinez454gmail.com>
2024-09-18 16:57:23 -0300

    usamos el supabase y lo configuramos con informacion nuestra del github

--- blogg/vite-project/src/App.jsx
+++ blogg/vite-project/src/App.jsx
@@ -28,6 +28,48 @@ function App() {
   )
 }
 
-export default App
+import default App
+
+export default function Header(){
+    return
+  
+}
+
+export defoult function App(){
+  const [user, setUser] = useState(null);
+
+  useEffect(()=>{
+    const getSession = async () =>{
+      const {data, error} = await supabase.auth.getSession();
+      if (error){
+        console.log(error);
+      } else {
+        setUser(data?.session?.user):
+      }
+    };
+
+    getSession();
+  },[]);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+}
 
 
+const handleClick = async () => {
+  const { data, error } = supabase, auth.signInWith0Auth({
+    provider:"github",
+  }),
+  if (error) {
+    console.log(error);
+    }else {
+    console.log(data);  
+    }
+  };
+return (
+  <>
+    
+    <header>
+      este es el header
+      <button >Conectar con Hithub</button>
+    </header>
+  </>
+ );
+)
